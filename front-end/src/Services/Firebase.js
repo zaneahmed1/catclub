3e3c040537254d72dc3c97aaaaf5def414b913ee
Zane <zaneahmed4@gmail.com>
2022-09-11 11:27:11 -0400

    ready to deploy again

--- front-end/src/Services/Firebase.js
+++ front-end/src/Services/Firebase.js
@@ -17,7 +17,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth();
 auth.useDeviceLanguage();
